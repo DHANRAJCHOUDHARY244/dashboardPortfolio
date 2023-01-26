44a7bd21a7530a767af664fe5b92c0438afcb1cc
DHANRAJCHOUDHARY244 <86722855+DHANRAJCHOUDHARY244@users.noreply.github.com>
2023-01-26 15:51:44 +0530

    setup

---
+++ mail.php
@@ -0,0 +1,17 @@
+<?php
+
+$recepient = 'your.mail@here.com';
+$sitename = 'Dhanraj';
+
+$name = trim($_POST['name']);
+$email = trim($_POST['email']);
+$text = trim($_POST['text']);
+$message = "Name: $name \nEmail: $email \nText: $text";
+
+$pagetitle = "New message from the \"$sitename\"";
+mail(
+    $recepient,
+    $pagetitle,
+    $message,
+    "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient"
+);
